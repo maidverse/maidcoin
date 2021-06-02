028dc1739e3fe2e945c62ecd0cd150dede88b4b7
Dr. Ada <contact@maidcoin.org>
2021-06-02 09:32:41 +0900

    add interface

---
+++ MaidItem.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./MaidItemInterface.sol";
+
+contract MaidItem is MaidItemInterface {
+
+    mapping(uint256 => mapping(address => uint256)) private balances;
+    mapping(address => mapping(address => bool)) private allowed;
+
+    function safeTransferFrom(address from, address to, uint256 id, uint256 value, bytes calldata data) override external {
+
+    }
+
+    function safeBatchTransferFrom(address from, address to, uint256[] calldata ids, uint256[] calldata values, bytes calldata data) override external {
+
+    }
+    
+    function balanceOf(address owner, uint256 id) override external view returns (uint256) {
+        return balances[id][owner];
+    }
+    
+    function balanceOfBatch(address[] calldata owners, uint256[] calldata ids) override external view returns (uint256[] memory) {
+
+    }
+    
+    function setApprovalForAll(address operator, bool approved) override external {
+
+    }
+    
+    function isApprovedForAll(address owner, address operator) override external view returns (bool) {
+
+    }
+}
---
+++ MasterCoinInterface.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+interface MasterCoinInterface {
+
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
+    event Burn(address indexed owner, uint256 amount);
+    event Mint(address indexed owner, uint256 amount);
+
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address owner) external view returns (uint256 balance);
+    function transfer(address to, uint256 amount) external returns (bool success);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool success);
+    function approve(address spender, uint256 amount) external returns (bool success);
+    function allowance(address owner, address spender) external view returns (uint256 remaining);
+
+    function burn(address to, uint256 amount) external;
+    function mint(uint256 amount) external;
+}
