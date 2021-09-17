e90874e14ea26bd64cb01ebd8a8611b0b59cf659
TheGreatHB <kavanaboy@gmail.com>
2021-09-17 14:30:34 +0900

    Housekeeping

--- contracts/interfaces/ICloneNurses.sol
+++ contracts/interfaces/ICloneNurses.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.5;
+pragma solidity >=0.5.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
-import "../interfaces/ICloneNurseEnumerable.sol";
+import "./ICloneNurseEnumerable.sol";
 import "./ISupportable.sol";
 import "./INursePart.sol";
 import "./IMaidCoin.sol";
